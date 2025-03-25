b87f82d5150fe1d22e286cf6bb0f058307c1a92b
Anil Kushwaha <113308156+Anil-Kushwaha-2002@users.noreply.github.com>
2025-03-25 16:01:40 +0530

    Delete task_management directory

--- task_management/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UserListView  # Import a dummy view to prevent errors
-
-urlpatterns = [
-    path('', UserListView.as_view(), name='user-list'),
-]
--- task_management/users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
-from .models import User
-from .serializers import UserSerializer
-
-class UserListView(ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
